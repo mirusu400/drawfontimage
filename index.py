0ca9ffabb52b6666387537b0e62f426b23017739
미루나무 <mirusu400@naver.com>
2021-01-16 02:12:45 +0000

    add rreader

--- index.py
+++ index.py
@@ -3,37 +3,73 @@ from PyQt5.QtWidgets import (
     QLabel,
     QPushButton,
     QVBoxLayout,
+    QMainWindow,
     QApplication,
-    QWidget
+    QAction,
+    QWidget,
+    qApp
+)
+from PyQt5.QtGui import (
+    QIcon
 )
 import sys
 import core
 
 
-class Dialog(QDialog):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(Dialog, self).__init__()
+        super().__init__()
         self.initUI()
+
+    def initMenu(self):
+        exitAction = QAction('Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+        self.statusBar()
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
 
     def initUI(self):
         self.resize(800, 600)
-        self.button = QPushButton("Click me")
-        self.button.clicked.connect(self.onClick)
-        self.label = QLabel("", self)
-        mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.button)
-        mainLayout.addWidget(self.label)
-        self.setLayout(mainLayout)
-        self.setWindowTitle("Example")
+        self.initMenu()
+        btn = QPushButton('button01', self)
+        btn.move(100,100)
+        self.setWindowTitle("FontEditor")
+        self.show()
 
     def onClick(self):
         self.label.setText("Button clicked")
 
+
+class Widgets(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        label1 = QLabel('Label1', self)
+        label1.move(20, 20)
+        label2 = QLabel('Label2', self)
+        label2.move(20, 60)
+
+        btn1 = QPushButton('Button1', self)
+        btn1.move(80, 13)
+        btn2 = QPushButton('Button2', self)
+        btn2.move(80, 53)
+
+        self.setWindowTitle('Absolute Positioning')
+        self.setGeometry(300, 300, 400, 200)
 
 if __name__ == '__main__':
     cv = core.canvas(font="example/NanumGothic.ttf", mode="RGB", size=16)
     cv.create("귓규균", output="output3.png", mode="n")
     cv.posterize_palette()
     app = QApplication(sys.argv)
-    dialog = Dialog()
-    dialog.exec_()
+    widget = Widgets()
+    window = MainWindow()
+    sys.exit(app.exec_())
