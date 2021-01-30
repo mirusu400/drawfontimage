7e7deb43053b7c0d4b6674a8419c2f7eff93c8ec
미루나무 <mirusu400@naver.com>
2021-01-30 07:00:10 +0000

    Add GUI design

--- index.py
+++ index.py
@@ -12,58 +12,17 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import (
     QIcon
 )
+from PyQt5 import uic
 import sys
 import core
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
+form_class = uic.loadUiType("./form/form.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self) :
         super().__init__()
-        self.initUI()
-
-    def initMenu(self):
-        exitAction = QAction('Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
-        self.statusBar()
-
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-
-        filemenu = menubar.addMenu('&File')
-        filemenu.addAction(exitAction)
-
-    def initUI(self):
-        self.resize(800, 600)
-        self.initMenu()
-        btn = QPushButton('button01', self)
-        btn.move(100,100)
-        self.setWindowTitle("FontEditor")
-        self.show()
-
-    def onClick(self):
-        self.label.setText("Button clicked")
-
-
-class Widgets(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        label1 = QLabel('Label1', self)
-        label1.move(20, 20)
-        label2 = QLabel('Label2', self)
-        label2.move(20, 60)
-
-        btn1 = QPushButton('Button1', self)
-        btn1.move(80, 13)
-        btn2 = QPushButton('Button2', self)
-        btn2.move(80, 53)
-
-        self.setWindowTitle('Absolute Positioning')
-        self.setGeometry(300, 300, 400, 200)
+        self.setupUi(self)
 
 
 if __name__ == '__main__':
@@ -73,7 +32,7 @@ if __name__ == '__main__':
     cv.change_palette("./example/TLP.pal")
     cv.save("output3.png")
     # cv.posterize_palette()
-    # app = QApplication(sys.argv)
-    # widget = Widgets()
-    # window = MainWindow()
-    # sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
